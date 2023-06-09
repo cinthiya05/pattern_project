cb586b0ade32a2ddc19fd3b43cf61c4a065d677e
cinthiya <cinthiyamurugan@gmail.com>
2023-06-09 20:59:24 +0530

    pattern program

---
+++ pattern1.py
@@ -0,0 +1,11 @@
+def triangle(f):
+    k = f - 1
+    for i in range (0,f):
+        for j in range(0,k):
+            print(end=" ")
+        k = k - 1
+        for j in range(0,i+1):
+            print("*", end=" ")
+        print("\r")
+f = 6
+triangle(f)
