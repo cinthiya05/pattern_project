9e9c61b751f540d8a34dcef8ce03da20fe8b4e04
cinthiya <cinthiyamurugan@gmail.com>
2023-06-13 14:03:36 +0530

    reverse of L pattern program

--- test.py
+++ test.py
@@ -36,18 +36,19 @@
 #        print("*",end=" ")
 #    print(" ")
 
+
 #row = 6
 #for i in range(row):
-#    print(' '* i,end=" ")
-#    for j in range(row - i):
+#    print(' ' * i,end=" ")
+#    for j in range(row-i):
 #        print("*",end=" ")
 #    print()
 
-
-row = 6
-for i in range(row):
-    print(' ' * i,end=" ")
-    for j in range(row-i):
-        print("*",end=" ")
-    print()
-
+def triangle(rows):
+    for i in range( rows, 0, -1):
+        for j in range(rows - i):
+            print("  " ,end="")
+        for k in range(i):
+            print("* ", end="")
+        print() 
+triangle(6)
